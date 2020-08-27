2b19bb6e84cc4895397742a9c77988f40c27a808
sswu <sswuai@gmail.com>
2020-08-27 13:48:28 +0800

    Delete eva_KL_op.sh

--- test_evaluation/eva_KL_op.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-username=$1
-modelname=$2
-gpu_ids=$3
-echo "username=$1"
-echo "modelname=$2"
-
-CUDA_VISIBLE_DEVICES=$gpu_ids python Evaluation/KL_model_data.py /home/$username/codelab/datasets/opcrossview/pano_test  \
-/home/$username/codelab/I2I/panogan_test_results/$modelname/test_latest/images #| tee eva_KL_$modelname.txt
