1239a43a5eb126e41c5746a896c007cfe0bda049
sswu <sswuai@gmail.com>
2020-08-27 13:49:39 +0800

    Delete eva_fid.sh

--- test_evaluation/eva_fid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-username=$1
-modelname=$2
-gpu_ids=$3
-echo "username=$1"
-echo "modelname=$2"
-
-python Evaluation/pytorch-fid/fid_score.py --gpu $gpu_ids /home/$username/codelab/datasets/db_crossview_pano_sep/test/test_pano  \
-/home/$username/codelab/I2I/panogan_test_results/$modelname/test_latest/images | tee eva_fid_$modelname.txt
